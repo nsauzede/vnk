8fd295d5b8a5d4a406b65563cd28e1a6ab120844
Nicolas Sauzede <nsauzede@laposte.net>
2021-04-22 01:34:56 +0200

    Fix build with newer V

--- examples/mainnk_v/mainnk_v.v
+++ examples/mainnk_v/mainnk_v.v
@@ -4,103 +4,101 @@
 // Use of this source code is governed by an MIT license
 // that can be found in the LICENSE file.
 module main
+
 import nsauzede.vsdl2 as sdl
 import nsauzede.vnk
 import os
 import time
 
 const (
-	window_width = 400
-	window_height = 400
-	max_vertex_memory = 512 * 1024
+	window_width       = 400
+	window_height      = 400
+	max_vertex_memory  = 512 * 1024
 	max_element_memory = 128 * 1024
 )
 
 enum Op {
-	easy hard
+	easy
+	hard
 }
 
 struct AppState {
 mut:
 	hide_window bool
-	nkw_rect C.nk_rect = C.nk_rect{50, 50, 230, 250}
-	bg C.nk_colorf
-	last_time int
-	frames int
-	fps int
-	win voidptr
-	ctx voidptr
-	win_width int
-	win_height int
-	op Op
-	property int
+	nkw_rect    C.nk_rect = C.nk_rect{50, 50, 230, 250}
+	bg          C.nk_colorf
+	last_time   int
+	frames      int
+	fps         int
+	win         voidptr
+	ctx         voidptr
+	win_width   int
+	win_height  int
+	op          Op
+	property    int
 }
 
 // these kludges are workaround for "the following imports were never used"
 // until these annoyances are fixed
 const (
-  vnk_version = vnk.version
-  sdl_version = sdl.version
-  os_maxpath = os.max_path_len
+	vnk_version = vnk.version
+	sdl_version = sdl.version
+	os_maxpath  = os.max_path_len
 )
 
 [live]
 fn (mut s AppState) live_main() {
 	if !s.hide_window {
-	if 1 == C.nk_begin(s.ctx, "Hello, Vorld! VVVV", s.nkw_rect, 0
-//		| C.NK_WINDOW_BORDER
-		| C.NK_WINDOW_MOVABLE
-		| C.NK_WINDOW_SCALABLE
-		| C.NK_WINDOW_MINIMIZABLE
-		| C.NK_WINDOW_TITLE
-		) {
-		s.nkw_rect = C.nk_window_get_bounds(s.ctx)
+		flags := 0 | C.NK_WINDOW_MOVABLE | C.NK_WINDOW_SCALABLE | C.NK_WINDOW_MINIMIZABLE | C.NK_WINDOW_TITLE
+		//| C.NK_WINDOW_BORDER
+		if 1 == C.nk_begin(s.ctx, c'Hello, Vorld! VVVV', s.nkw_rect, flags) {
+			s.nkw_rect = C.nk_window_get_bounds(s.ctx)
 
-		C.nk_layout_row_static(s.ctx, 30, 80, 1)
+			C.nk_layout_row_static(s.ctx, 30, 80, 1)
 
-		if 1 == C.nk_button_label(s.ctx, "Click Me!") {
-			mode := if s.op == .easy {"Easy"} else {"Hard"}
-			r := s.bg.r * 255
-			g := s.bg.g * 255
-			b := s.bg.b * 255
-			println('button pressed! mode=$mode compr=$s.property r=$r g=$g b=$b')
-		}
-		C.nk_layout_row_dynamic(s.ctx, 30, 2)
-		if 1 == C.nk_option_label(s.ctx, "easy", s.op == .easy) {
-			s.op = .easy
-		}
-		if 1 == C.nk_option_label(s.ctx, "hard", s.op == .hard) {
-			s.op = .hard
-		}
-		C.nk_layout_row_dynamic(s.ctx, 22, 1)
-		C.nk_property_int(s.ctx, "Compression:", 0, &s.property, 100, 10, 1)
+			if 1 == C.nk_button_label(s.ctx, c'Click Me!') {
+				mode := if s.op == .easy { 'Easy' } else { 'Hard' }
+				r := s.bg.r * 255
+				g := s.bg.g * 255
+				b := s.bg.b * 255
+				println('button pressed! mode=$mode compr=$s.property r=$r g=$g b=$b')
+			}
+			C.nk_layout_row_dynamic(s.ctx, 30, 2)
+			if 1 == C.nk_option_label(s.ctx, c'easy', s.op == .easy) {
+				s.op = .easy
+			}
+			if 1 == C.nk_option_label(s.ctx, c'hard', s.op == .hard) {
+				s.op = .hard
+			}
+			C.nk_layout_row_dynamic(s.ctx, 22, 1)
+			C.nk_property_int(s.ctx, c'Compression:', 0, &s.property, 100, 10, 1)
 
-		t := time.now().unix
-		s.frames++
-		fps := 'Application average $s.fps FPS'
-		if t > s.last_time {
-			s.fps = s.frames
-			s.frames = 0
-		}
-		s.last_time = int(t)
-		C.nk_label(s.ctx, fps.str, C.NK_TEXT_LEFT)
+			t := time.now().unix
+			s.frames++
+			fps := 'Application average $s.fps FPS'
+			if t > s.last_time {
+				s.fps = s.frames
+				s.frames = 0
+			}
+			s.last_time = int(t)
+			C.nk_label(s.ctx, fps.str, C.NK_TEXT_LEFT)
 
-		C.nk_layout_row_dynamic(s.ctx, 20, 1)
-		C.nk_label(s.ctx, "background:", C.NK_TEXT_LEFT)
-		C.nk_layout_row_dynamic(s.ctx, 25, 1)
-		size := C.nk_vec2{C.nk_widget_width(s.ctx),400}
-		if 1 == C.nk_combo_begin_color(s.ctx, C.nk_rgb_cf(s.bg), size) {
-			C.nk_layout_row_dynamic(s.ctx, 120, 1)
-			s.bg = C.nk_color_picker(s.ctx, s.bg, C.NK_RGBA)
+			C.nk_layout_row_dynamic(s.ctx, 20, 1)
+			C.nk_label(s.ctx, c'background:', C.NK_TEXT_LEFT)
 			C.nk_layout_row_dynamic(s.ctx, 25, 1)
-			s.bg.r = C.nk_propertyf(s.ctx, "#R:", 0, s.bg.r, 1.0, 0.01, 0.005)
-			s.bg.g = C.nk_propertyf(s.ctx, "#G:", 0, s.bg.g, 1.0, 0.01, 0.005)
-			s.bg.b = C.nk_propertyf(s.ctx, "#B:", 0, s.bg.b, 1.0, 0.01, 0.005)
-			s.bg.a = C.nk_propertyf(s.ctx, "#A:", 0, s.bg.a, 1.0, 0.01, 0.005)
-			C.nk_combo_end(s.ctx)
+			size := C.nk_vec2{C.nk_widget_width(s.ctx), 400}
+			if 1 == C.nk_combo_begin_color(s.ctx, C.nk_rgb_cf(s.bg), size) {
+				C.nk_layout_row_dynamic(s.ctx, 120, 1)
+				s.bg = C.nk_color_picker(s.ctx, s.bg, C.NK_RGBA)
+				C.nk_layout_row_dynamic(s.ctx, 25, 1)
+				s.bg.r = C.nk_propertyf(s.ctx, c'#R:', 0, s.bg.r, 1.0, 0.01, 0.005)
+				s.bg.g = C.nk_propertyf(s.ctx, c'#G:', 0, s.bg.g, 1.0, 0.01, 0.005)
+				s.bg.b = C.nk_propertyf(s.ctx, c'#B:', 0, s.bg.b, 1.0, 0.01, 0.005)
+				s.bg.a = C.nk_propertyf(s.ctx, c'#A:', 0, s.bg.a, 1.0, 0.01, 0.005)
+				C.nk_combo_end(s.ctx)
+			}
 		}
-	}
-	C.nk_end(s.ctx)
+		C.nk_end(s.ctx)
 	}
 	C.SDL_GetWindowSize(s.win, &s.win_width, &s.win_height)
 	C.glViewport(0, 0, s.win_width, s.win_height)
@@ -111,17 +109,19 @@ fn (mut s AppState) live_main() {
 }
 
 fn main() {
-	mut s := AppState{win:0 ctx:0}
+	mut s := AppState{
+		win: 0
+		ctx: 0
+	}
 	s.last_time = int(time.now().unix)
 
-	C.SDL_SetHint(C.SDL_HINT_VIDEO_HIGHDPI_DISABLED, "0")
-	C.SDL_Init(C.SDL_INIT_VIDEO|C.SDL_INIT_TIMER|C.SDL_INIT_EVENTS)
-	s.win = C.SDL_CreateWindow("Live! V Nuklear+SDL2+OpenGL3 demo",
-	C.SDL_WINDOWPOS_CENTERED, C.SDL_WINDOWPOS_CENTERED,
-	window_width, window_height, C.SDL_WINDOW_OPENGL|C.SDL_WINDOW_SHOWN|C.SDL_WINDOW_ALLOW_HIGHDPI)
+	C.SDL_SetHint(C.SDL_HINT_VIDEO_HIGHDPI_DISABLED, '0')
+	C.SDL_Init(C.SDL_INIT_VIDEO | C.SDL_INIT_TIMER | C.SDL_INIT_EVENTS)
+	s.win = C.SDL_CreateWindow('Live! V Nuklear+SDL2+OpenGL3 demo', C.SDL_WINDOWPOS_CENTERED,
+		C.SDL_WINDOWPOS_CENTERED, window_width, window_height, C.SDL_WINDOW_OPENGL | C.SDL_WINDOW_SHOWN | C.SDL_WINDOW_ALLOW_HIGHDPI)
 	gl_context := C.SDL_GL_CreateContext(s.win)
-	C.SDL_GL_SetAttribute (C.SDL_GL_CONTEXT_FLAGS, C.SDL_GL_CONTEXT_FORWARD_COMPATIBLE_FLAG)
-	C.SDL_GL_SetAttribute (C.SDL_GL_CONTEXT_PROFILE_MASK, C.SDL_GL_CONTEXT_PROFILE_CORE)
+	C.SDL_GL_SetAttribute(C.SDL_GL_CONTEXT_FLAGS, C.SDL_GL_CONTEXT_FORWARD_COMPATIBLE_FLAG)
+	C.SDL_GL_SetAttribute(C.SDL_GL_CONTEXT_PROFILE_MASK, C.SDL_GL_CONTEXT_PROFILE_CORE)
 	C.SDL_GL_SetAttribute(C.SDL_GL_CONTEXT_MAJOR_VERSION, 3)
 	C.SDL_GL_SetAttribute(C.SDL_GL_CONTEXT_MINOR_VERSION, 3)
 	C.SDL_GL_SetAttribute(C.SDL_GL_DOUBLEBUFFER, 1)
@@ -134,23 +134,22 @@ fn main() {
 		exit(1)
 	}
 	s.ctx = C.nk_sdl_init(s.win)
-	/* Load Fonts: if none of these are loaded a default font will be used  */
-	/* Load Cursor: if you uncomment cursor loading please hide the cursor */
+	// Load Fonts: if none of these are loaded a default font will be used
+	// Load Cursor: if you uncomment cursor loading please hide the cursor
 	{
-		//struct nk_font_atlas *atlas
+		// struct nk_font_atlas *atlas
 		atlas := voidptr(0)
 		C.nk_sdl_font_stash_begin(&atlas)
-		/*struct nk_font *droid = nk_font_atlas_add_from_file(atlas, "../../../extra_font/DroidSans.ttf", 14, 0);*/
-		/*struct nk_font *roboto = nk_font_atlas_add_from_file(atlas, "../../../extra_font/Roboto-Regular.ttf", 16, 0);*/
-		/*struct nk_font *future = nk_font_atlas_add_from_file(atlas, "../../../extra_font/kenvector_future_thin.ttf", 13, 0);*/
-		/*struct nk_font *clean = nk_font_atlas_add_from_file(atlas, "../../../extra_font/ProggyClean.ttf", 12, 0);*/
-		/*struct nk_font *tiny = nk_font_atlas_add_from_file(atlas, "../../../extra_font/ProggyTiny.ttf", 10, 0);*/
-		/*struct nk_font *cousine = nk_font_atlas_add_from_file(atlas, "../../../extra_font/Cousine-Regular.ttf", 13, 0);*/
+		// struct nk_font *droid = nk_font_atlas_add_from_file(atlas, "../../../extra_font/DroidSans.ttf", 14, 0);
+		// struct nk_font *roboto = nk_font_atlas_add_from_file(atlas, "../../../extra_font/Roboto-Regular.ttf", 16, 0);
+		// struct nk_font *future = nk_font_atlas_add_from_file(atlas, "../../../extra_font/kenvector_future_thin.ttf", 13, 0);
+		// struct nk_font *clean = nk_font_atlas_add_from_file(atlas, "../../../extra_font/ProggyClean.ttf", 12, 0);
+		// struct nk_font *tiny = nk_font_atlas_add_from_file(atlas, "../../../extra_font/ProggyTiny.ttf", 10, 0);
+		// struct nk_font *cousine = nk_font_atlas_add_from_file(atlas, "../../../extra_font/Cousine-Regular.ttf", 13, 0);
 		C.nk_sdl_font_stash_end()
-		/*nk_style_load_all_cursors(ctx, atlas->cursors);*/
-		/*nk_style_set_font(ctx, &roboto->handle);*/
+		// nk_style_load_all_cursors(ctx, atlas->cursors);
+		// nk_style_set_font(ctx, &roboto->handle);
 	}
-
 	s.op = .easy
 	s.property = 20
 	bg := C.nk_colorf{0.10, 0.18, 0.24, 1.0}
@@ -160,12 +159,12 @@ fn main() {
 		evt := sdl.Event{}
 		C.nk_input_begin(s.ctx)
 		for C.SDL_PollEvent(&evt) > 0 {
-			if int(evt.@type) == C.SDL_QUIT {
+			if int(unsafe { evt.@type }) == C.SDL_QUIT {
 				running = false
-				goto cleanup
+				break
 			}
-			if evt.@type == C.SDL_KEYDOWN {
-				if evt.key.keysym.sym == C.SDLK_l {
+			if unsafe { evt.@type } == C.SDL_KEYDOWN {
+				if unsafe { evt.key.keysym.sym } == C.SDLK_l {
 					s.hide_window = !s.hide_window
 				}
 			}
@@ -176,7 +175,7 @@ fn main() {
 		s.live_main()
 	}
 
-cleanup:
+	cleanup:
 	C.nk_sdl_shutdown()
 	C.SDL_GL_DeleteContext(gl_context)
 	C.SDL_DestroyWindow(s.win)
--- vnk.v
+++ vnk.v
@@ -32,7 +32,8 @@ pub mut:
 	b f32
 	a f32
 }
-//pub type nk_colorf C.nk_colorf
+
+// pub type nk_colorf C.nk_colorf
 pub struct C.nk_color {
 pub mut:
 	r byte
@@ -40,6 +41,7 @@ pub mut:
 	b byte
 	a byte
 }
+
 pub struct C.nk_vec2 {
 pub mut:
 	x f32
@@ -52,12 +54,13 @@ pub struct C.nk_rect {
 	w f32
 	h f32
 }
-pub struct C.nk_image{
+
+pub struct C.nk_image {
 pub:
-handle voidptr
-w u16
-h u16
-region [4]u16
+	handle voidptr
+	w      u16
+	h      u16
+	region [4]u16
 }
 
 fn C.glGenTextures(n u32, textures u32)
@@ -69,23 +72,23 @@ fn C.nk_subimage_id(id int, w u16, h u16, sub_region C.nk_rect) C.nk_image
 fn C.nk_window_get_canvas(ctx voidptr) voidptr
 fn C.nk_draw_image(ptr voidptr, rect C.nk_rect, img voidptr, col C.nk_color)
 fn C.nk_image_color(ctx voidptr, img C.nk_image, col C.nk_color)
-fn C.nk_button_image_label(ctx voidptr, img C.nk_image, label charptr, text_alignment int) bool
+fn C.nk_button_image_label(ctx voidptr, img C.nk_image, label &char, text_alignment int) bool
 fn C.nk_image_id(tex int) C.nk_image
 fn C.nk_rgb_cf(c C.nk_colorf) C.nk_color
 fn C.nk_widget_width(ctx voidptr) f32
 fn C.nk_combo_begin_color(ctx voidptr, color C.nk_color, size C.nk_vec2) int
 fn C.nk_color_picker(ctx voidptr, colorf C.nk_colorf, cfmt int) C.nk_colorf
-fn C.nk_propertyf(ctx voidptr, name byteptr, min f32, val f32, max f32, step f32, inc_per_pixel f32) f32
+fn C.nk_propertyf(ctx voidptr, name &byte, min f32, val f32, max f32, step f32, inc_per_pixel f32) f32
 fn C.nk_combo_end(ctx voidptr)
 fn C.nk_rect(x f32, y f32, w f32, h f32) C.nk_rect
-fn C.nk_begin(ctx voidptr, title byteptr, bounds NkRect, flags int) int
+fn C.nk_begin(ctx voidptr, title &byte, bounds C.nk_rect, flags int) int
 fn C.nk_layout_row_static(ctx voidptr, height f32, item_width int, cols int)
-fn C.nk_button_label(ctx voidptr, title byteptr) int
+fn C.nk_button_label(ctx voidptr, title &byte) int
 fn C.nk_layout_row_dynamic(ctx voidptr, height f32, cols int)
-fn C.nk_option_label(ctx voidptr, title byteptr, active int) int
-fn C.nk_property_int(ctx voidptr, name byteptr, min int, val voidptr, max int, step int, inc_per_pixel f32)
-fn C.nk_label(ctx voidptr, title byteptr, align int)
-fn C.nk_text(ctx voidptr, title byteptr, flags int)
+fn C.nk_option_label(ctx voidptr, title &byte, active int) int
+fn C.nk_property_int(ctx voidptr, name &byte, min int, val voidptr, max int, step int, inc_per_pixel f32)
+fn C.nk_label(ctx voidptr, title &byte, align int)
+fn C.nk_text(ctx voidptr, title &byte, flags int)
 fn C.nk_end(ctx voidptr)
 fn C.nk_sdl_render(nk_anti_aliasing int, max_vertex_buffer int, max_element_buffer int)
 fn C.nk_sdl_init(win voidptr) voidptr
@@ -95,15 +98,15 @@ fn C.nk_input_begin(ctx voidptr)
 fn C.nk_sdl_handle_event(evt voidptr) int
 fn C.nk_input_end(ctx voidptr)
 fn C.nk_sdl_shutdown()
-fn C.glViewport()
-fn C.glClear()
-fn C.glClearColor()
+fn C.glViewport(x int, y int, width int, height int)
+fn C.glClear(mask int)
+fn C.glClearColor(red f32, green f32, blue f32, alpha f32)
 fn C.glewInit() int
-fn C.nk_window_get_bounds() C.nk_rect
-fn C.nk_popup_begin(nk_context voidptr, nk_popup_type int, charp charptr, nk_flags int, bounds C.nk_rect) bool
+fn C.nk_window_get_bounds(ctx voidptr) C.nk_rect
+fn C.nk_popup_begin(nk_context voidptr, nk_popup_type int, charp &char, nk_flags int, bounds C.nk_rect) bool
 fn C.nk_popup_end(nk_context voidptr)
-fn C.nk_font_atlas_add_from_file(atlas voidptr, file_path charptr, height f32, config voidptr) voidptr
+fn C.nk_font_atlas_add_from_file(atlas voidptr, file_path &char, height f32, config voidptr) voidptr
 
 pub const (
-  version = '0.1' // hack to avoid unused module warning in the main program
+	version = '0.1' // hack to avoid unused module warning in the main program
 )
